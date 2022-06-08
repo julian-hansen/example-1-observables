1f6e4b4cf5b7ed873711484ff3c4f7df5a457e40
julian@marcorpsa.com <julian@marcorpsa.com>
2022-06-08 11:07:01 +0200

    Forms and switchmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,26 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import { interval, Observable, of, Subject, Subscription } from 'rxjs';
+import { interval, Observable, ObservedValueOf, of, Subject, Subscription } from 'rxjs';
 import { ClientService } from './services/client.service';
-import { concatMap, delay, map, mergeMap, take} from 'rxjs/operators'
+import { concatMap, delay, map, mergeMap, switchMap, take, takeUntil} from 'rxjs/operators'
 import { Client } from './class/Client';
+import { FormControl, FormGroup } from '@angular/forms';
+
+const data = [
+  {color: 'Blue', msg: 'You selected blue as your favourite colour'},
+  {color: 'Red' , msg: 'Do you really like red that much'},
+  {color: 'Green', msg: 'The colour of trees, nice!'},
+  {color: 'White', msg : 'White are you talking about?'}
+]
+
+const getMessage = (colour:string) : Observable<string | null> => {
+  const result = data.find(item => item.color == colour)
+
+  if (result) {
+    return of(result.msg)
+  }
+
+  return of(null)
+}
 
 @Component({
   selector: 'app-root',
@@ -14,15 +32,35 @@ export class AppComponent implements OnInit, OnDestroy{
   public client$: Observable<Client> = of(new Client())
   public subClient$: Observable<Client> = of(new Client())
   public subscribedClient : Client | null = new Client()
+  public message$: Observable<string | null> = of(null)
+  public otherMessage: string | null = ''
+
   public name: string = ''
   public email = ''
+  public options = ["Red", "Blue", "Green", "White"];
   
   public showMe = false
   public sub: Subscription | null = null
   
+  public fg = new FormGroup({
+    options: new FormControl(''),
+    firstname: new FormControl('')
+  });
+
   constructor(private svc: ClientService) {}
 
+  get optionsCtrl() : FormControl {
+    return this.fg.get('options') as FormControl
+  }
+
+  get nametrl() : FormControl {
+    return this.fg.get('firstname') as FormControl
+  }
+
   ngOnInit(): void {
+
+    this.nametrl.patchValue('Matthew')
+
     this.client$ = this.svc.getClientSelector()
     this.subClient$ = this.svc.getSubjectClientSelector()
     //this.sub = this.svc.getClientSelector().subscribe(client => console.log({client}))
@@ -40,49 +78,25 @@ export class AppComponent implements OnInit, OnDestroy{
       console.log(`With concatMap: ${val}`)
     );
     
-
-    // showing the difference between concatMap and mergeMap
-    const mergeMapExample = source
-      .pipe(
-        // just so we can log this after the first example has run
-        delay(5000),
-        mergeMap(val => of(`Delayed by: ${val}ms`).pipe(delay(val)))
-      )
-      .subscribe(val => console.log(`With mergeMap: ${val}`));    
-
-    const test:Observable<any> = of([1,2,3], "dog", 25)
-    const timer = interval(1000).pipe(take(2))
-    test
-    .pipe(
-      concatMap((a:any) => timer
+    /*the non-switchmap way */
+    this.optionsCtrl.valueChanges
+    .pipe (
+      takeUntil(this.destroy$)
+    ).subscribe(val => {
+      getMessage(val)
         .pipe(
-          map((i:number) => i + ' => ' + a)
-          )
-      )
-    ).subscribe(res => console.log('---------------->', res))
+          takeUntil(this.destroy$)
+        )
+        .subscribe(val => this.otherMessage = val)
+    })
 
-    test
+    /* Using switchmap */
+    this.message$ = this.optionsCtrl.valueChanges
     .pipe(
-      mergeMap(a => timer
-        .pipe(
-          map(i => i + ' => ' + a)
-          )
-      )
-      ).subscribe(res => console.log('=================>', res))
-    
-
-/*    
-    test
-    .pipe(
-      take(2),
-      mergeMap(a => interval(1000).pipe(
-        take(5),
-        map(i => i.toString() + ' => ' + a)
-      ))
+      takeUntil(this.destroy$),
+      switchMap(val => getMessage(val))
     )
-    .subscribe(a => console.log(a))
-*/
-  }
+  } 
 
   killSub() {
     if (this.sub){
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ClientViewComponent } from './components/client-view/client-view.component';
@@ -16,6 +17,7 @@ import { ClientViewStandaloneComponent } from './components/client-view-standalo
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
